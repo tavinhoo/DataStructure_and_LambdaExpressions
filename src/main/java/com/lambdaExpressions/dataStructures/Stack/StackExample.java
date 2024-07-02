f2fa0b4787045dce627251b9014e054aaeb82b0f
Luiz Otávio <luizotaviomtbs@gmail.com>
2024-07-01 21:46:44 -0300

    Stack structure example

--- src/main/java/com/lambdaExpressions/dataStructures/Stack/StackExample.java
+++ src/main/java/com/lambdaExpressions/dataStructures/Stack/StackExample.java
@@ -6,5 +6,29 @@ public class StackExample {
 
     public void run() {
 
+        Stack<Integer> stack = new Stack<>();
+
+        // Adicionando elementos à Stack
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        // Exibindo o conteúdo da Stack
+        System.out.println("Stack: " + stack);
+
+        // Removendo elementos da Stack
+        int removedElement = stack.pop();
+        System.out.println("Elemento removido: " + removedElement);
+        System.out.println("Stack após remoção: " + stack);
+
+        // Visualizando o elemento no topo da Stack
+        int topElement = stack.peek();
+        System.out.println("Elemento no topo: " + topElement);
+
+        // Verificando se a Stack está vazia
+        boolean isEmpty = stack.isEmpty();
+        System.out.println("A Stack está vazia? " + isEmpty);
     }
 }
